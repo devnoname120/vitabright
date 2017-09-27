191dc690fd1ebc249cd8adf01084346f19bb4b84
devnoname120 <devnoname120+gh@gmail.com>
2017-09-27 18:39:03 +0200

    Generate lookup and use it

--- main.c
+++ main.c
@@ -4,58 +4,13 @@
 #include <psp2kern/io/fcntl.h>
 
 #include "log.h"
+#include "lookupNew.h"
 
-void _start() __attribute__ ((weak, alias ("module_start")));
-	
+
+int ksceDisplaySetBrightness(int unk, unsigned int brightness);
+
 //static tai_hook_ref_t hook_get_max_brightness;
 static tai_hook_ref_t hook_set_brightness;
-
-#define DEVICE_BASE         (0xE0000000)
-#define IFTU0_BASE (DEVICE_BASE + 0x5020000)
-
-
-uint *iftu0_base = NULL;
-
-static inline void turnon_oled(int a2, int a3)
-{
-    if (a2 == 128)
-    {
-        *(iftu0_base + 0x2000 + 32) = 0;
-        *(iftu0_base + 0x2000 + 4) &= ~1;
-    }
-    else
-    {
-        *(iftu0_base + 0x2000 + 32) = a2;
-        *(iftu0_base + 0x2000 + 4) |= 1;
-    }
-
-    if (a3 == 256)
-    {
-        *(iftu0_base + 0x1000 + 160) = 1;
-    }
-    else
-    {
-        *(iftu0_base + 0x1000 + 140) = a3;
-        *(iftu0_base + 0x1000 + 160) = 0;
-    }
-}
-
-int brightness_adjust(void)
-{
-    for (int j = 0; j < 256; j += 2)
-    {
-        turnon_oled(4, j);
-        for (int k = 0; k < 0x1000000; k++);
-    }
-    turnon_oled(128, 0);
-    for (int j = 254; j >= 0; j -= 2)
-    {
-        turnon_oled(4, j);
-        for (int k = 0; k < 0x1000000; k++);
-    }
-}
-
-
 
 /*int get_max_brightness(uint *brightness)
 {
@@ -77,34 +32,16 @@ int set_brightness(uint brightness)
 	else
 		mod_brightness = (brightness-19)*(65536)/(65536-19);
 	//int ret = TAI_CONTINUE(uint, hook_set_brightness, mod_brightness);
-	
-	//int intensity = (char)((brightness-20)*256/(65536-20));
+
 	LOG("set_brightness(%u), corrected to %u\n", brightness, mod_brightness);
 	
-	SceDisplayForDriver_0x9E3C6DC6(0, mod_brightness);
-
-	//brightness_adjust();
-	//turnon_oled(4, intensity);
-
+	ksceDisplaySetBrightness(0, mod_brightness);
 	return 0;
 }
 
-
+void _start() __attribute__ ((weak, alias ("module_start")));
 int module_start(SceSize argc, const void *args)
 {
-	/*SceKernelAllocMemBlockKernelOpt opt = {0};
-	opt.size = sizeof(opt);
-	opt.attr = 2;
-	opt.paddr = IFTU0_BASE;
-	int payload_block = ksceKernelAllocMemBlock("hw_regs", 0x20100206, 0x3000, &opt); // Need 0x2032 but it has to be page-aligned so 0x3000
-	if (payload_block < 0)
-		LOG("AllocMemBlock ret=0x%08X", payload_block);
-	int ret = ksceKernelGetMemBlockBase(payload_block, (void**)&iftu0_base);
-	
-	if (ret < 0)
-		LOG("GetMemBlock ret=0x%08X", ret);
-		
-	*/
 	LOG("vitabright started...\n");	
 
 	/*ret = taiHookFunctionExportForKernel(KERNEL_PID, &hook_get_max_brightness,
@@ -119,9 +56,23 @@ int module_start(SceSize argc, const void *args)
                                  TAI_ANY_LIBRARY,
                                  0xE0C1B743,
                                  set_brightness);
+	LOG("hook set_brightness: 0x%08X\n", ret);
 	
-	LOG("ret: 0x%08X\n", ret);
+	tai_module_info_t info;
+	info.size = sizeof(tai_module_info_t);
+	ret = taiGetModuleInfoForKernel(KERNEL_PID, "SceOled", &info);
+	LOG("getmodninfo: 0x%08X\n", ret);
+	LOG("modid: 0x%08X\n", info.modid);
+	
+	if (sizeof(lookupNew) != 357) {
+		LOG("size mismatch! Skipping...\n");
+		return SCE_KERNEL_START_SUCCESS;
+	}
 
+	LOG("Size ok, hooking...\n");
+	ret = taiInjectDataForKernel(KERNEL_PID, info.modid, 1, 0x1E00, lookupNew, sizeof(lookupNew));	
+	LOG("injectdata: 0x%08X\n", ret);
+	
 	return SCE_KERNEL_START_SUCCESS;
 }
 
